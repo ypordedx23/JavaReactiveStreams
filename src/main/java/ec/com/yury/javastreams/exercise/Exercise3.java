d17ac7d5393d5d5210f67afc4887c22d0106f567
ypordedx23 <yurymero2396@gmail.com>
2023-05-09 21:46:01 -0500

    Exercise Instructions

---
+++ src/main/java/ec/com/yury/javastreams/exercise/Exercise3.java
@@ -0,0 +1,17 @@
+package ec.com.yury.javastreams.exercise;
+
+import java.io.IOException;
+
+public class Exercise3 {
+
+    public static void main(String [] args) throws IOException {
+        // Use reactiveSources.intNumbersFlux()
+
+        //Get all numbers in the ReactiveSources.intNumbersFlux stream
+        //into a list and print the list and list size
+
+
+        System.out.println("Press a key to end");
+        System.in.read();
+    }
+}
