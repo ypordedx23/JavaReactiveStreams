74ef45affca679dd75c2749eaad8a2ba1baaf68c
ypordedx23 <yurymero2396@gmail.com>
2023-05-09 21:11:19 -0500

    Project Instrucctions

--- src/main/java/ec/com/yury/javastreams/exercise/Exercise2.java
+++ src/main/java/ec/com/yury/javastreams/exercise/Exercise2.java
@@ -1,6 +1,21 @@
 package ec.com.yury.javastreams.exercise;
 
+import java.io.IOException;
+
 public class Exercise2 {
 
+    public static void main (String []args) throws IOException {
+
+        //Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
+
+        //Print all numbers in the ReactiveSources.intNumbersFlux stream
+
+
+        //Print all users in the ReactiveSources.userFlux stream
+
+        System.out.println("Press a key to end");
+        System.in.read();
+
+    }
 
 }
